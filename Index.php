8f8a6417405720592e2f73da30cc39d51798aa39
GH1sto <123340654+GH1sto@users.noreply.github.com>
2025-04-14 19:42:35 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ahorrópolis - Juego Educativo Financiero</title>
+  <title>Ahorrópoli - Juego Educativo Financiero</title>
   <style>
     body {
       font-family: 'Segoe UI', sans-serif;
